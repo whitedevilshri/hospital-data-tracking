cbf13136bdffccac948d7665d31f258a03a337b6
whitedevilshri <shrikhanderay132@gmail.com>
2025-02-12 11:55:34 +0530

    Saved local changes before rebase

--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset(); // Clear all session variables
-session_destroy(); // Destroy the session
-header("Location: http://localhost/dbms_hms/index.php"); // Redirect to the home page
-exit();
-?>
